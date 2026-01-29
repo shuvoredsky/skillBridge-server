aa3f4b369351cb289d7b4ffe4ace5cf53cc5f265
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-30 01:50:35 +0600

    add availability on routes.ts

--- src/modules/availability/availability.route.ts
+++ src/modules/availability/availability.route.ts
@@ -1,6 +1,21 @@
 import express from "express"
+import auth, { UserRole } from "../../middleware/auth"
+import { AvailabilityController } from "./availability.controller"
 
 
 const router = express.Router()
 
 
+router.post("/", auth(UserRole.TUTOR), AvailabilityController.createAvailability);
+
+router.put("/:id", auth(UserRole.TUTOR),
+AvailabilityController.updateAvailability)
+
+router.delete("/:id", auth(UserRole.TUTOR),
+AvailabilityController.deleteAvailability)
+
+router.get("/:tutorId", AvailabilityController.getTutorAvailability)
+
+
+
+export const availabilityRouter = router;
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { userRouter } from "./modules/user/user.route";
 import { tutorRouter } from "./modules/tutor/tutor.route";
 import { categoryRouter } from "./modules/category/category.route";
+import { availabilityRouter } from "./modules/availability/availability.route";
 
 
 
@@ -11,6 +12,7 @@ const router = express.Router();
 router.use("/users", userRouter);
 router.use("/tutors", tutorRouter);
 router.use("/categories", categoryRouter)
+router.use("/availability", availabilityRouter)
 
 
 export default router;
