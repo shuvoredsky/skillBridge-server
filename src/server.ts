d0850a4e5dffca915a0529dff7c7ec1418e32ca1
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-02-03 03:04:39 +0600

    update packege.json

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
-    import app from "./app";
-    import { prisma } from "./lib/prisma"
+import app from "./app";
+import { prisma } from "./lib/prisma"
 
-    const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-    async function main(){
-        try{
-            await prisma.$connect();
-            console.log("Connected to the database successfully")
 
-            app.listen(port, ()=>{
-                console.log(`Server is running on http://localhost:${port}`)
-            })
-        }catch(error){
-            console.error("An error occurred", error)
-            await prisma.$disconnect();
-            process.exit(1)
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully");
+
+        
+        if (process.env.NODE_ENV !== 'production') {
+            app.listen(port, () => {
+                console.log(`Server is running on http://localhost:${port}`);
+            });
         }
+    } catch (error) {
+        console.error("An error occurred", error);
+        await prisma.$disconnect();
     }
+}
 
-    main();
+main();
+
+export default app;
